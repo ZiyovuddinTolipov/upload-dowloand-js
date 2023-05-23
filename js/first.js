da05d46b1d5e6e720f8a18b1ffc2e9d09a107067
Ziyovuddin <ziyovudin.developer@gmail.com>
2023-05-24 03:26:07 +0500

    navbar links

--- js/first.js
+++ js/first.js
@@ -4,7 +4,6 @@ const errText = document.querySelector('.err-text');
 // alert("Please enter");
 formFirst.addEventListener('submit', event => {
     event.preventDefault();
-
      const keyPage = document.getElementById("file-guid").value;
      if (keyPage == "6860-6564-2e7a-9161-c9b4-1871-8566") {
         // alert("hello")

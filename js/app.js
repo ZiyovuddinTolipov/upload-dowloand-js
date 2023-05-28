668988eca9e066ad5d6b4dfea0904ddbceb58798
tolipov <ziyovuddin.developer@gmail.com>
2023-05-28 10:49:39 +0500

    the end

--- js/app.js
+++ js/app.js
@@ -6,75 +6,83 @@ const errText = document.querySelector('.err-text');
 const errText2 = document.querySelector('.err-text-2');
 const errTextCaptcha = document.querySelector('.err-text-captcha');
 const chaptchaImg = document.querySelector('.chaptcha-1');
+const glyphicon = document.querySelector('.glyphicon');
 
-function randomNums() {
-    const valueIMG = [7, 8, 6, 4, 9, 4, 8, 1, 7, 3, 7];
-    const valueURL = [
-        "./images/captcha/random0.png",
-        "./images/captcha/random1.png",
-        "./images/captcha/random2.png",
-        "./images/captcha/random3.png",
-        "./images/captcha/random4.png",
-        "./images/captcha/random5.png",
-        "./images/captcha/random6.png",
-        "./images/captcha/random7.png",
-        "./images/captcha/random8.png",
-        "./images/captcha/random9.png",
-        "./images/captcha/random10.png"
-    ];
+var randomValue1;
 
-    const randomNum = Math.floor(Math.random() * 10)
-    let malumot = {
-        valueURL: valueURL[randomNum],
-        valueIMG: valueIMG[randomNum]
-    };
-    chaptchaImg.style.backgroundImage = `url('${malumot.valueURL}')`
-    // console.log(malumot,'first');
-    return malumot;
+const myArray = [
+    { valueURL: "./images/captcha/random0.png", valueIMG: 7 },
+    { valueURL: './images/captcha/random1.png', valueIMG: 8 },
+    { valueURL: './images/captcha/random2.png', valueIMG: 6 },
+    { valueURL: './images/captcha/random3.png', valueIMG: 4 },
+    { valueURL: './images/captcha/random4.png', valueIMG: 9 },
+    { valueURL: './images/captcha/random5.png', valueIMG: 4 },
+    { valueURL: './images/captcha/random6.png', valueIMG: 8 },
+    { valueURL: './images/captcha/random7.png', valueIMG: 1 },
+    { valueURL: './images/captcha/random8.png', valueIMG: 7 },
+    { valueURL: './images/captcha/random9.png', valueIMG: 3 },
+    { valueURL: './images/captcha/random10.png', valueIMG: 7 }
 
+];
+function randomNums(myArr) {
+    console.log(myArr);
+    var randomIndex = Math.floor(Math.random() * myArr.length);
+    randomValue1 = myArr[randomIndex];
+    chaptchaImg.style.backgroundImage = `url('${randomValue1.valueURL}')`
+    console.log(randomValue1, 'randomValue');
+    // location.reload()
+    // return randomValue;
 };
-// funksiya ichidagi malumotni tashqariga olib oldik
-const data = randomNums();
-console.log( data.valueIMG)
+randomNums(myArray);
+// console.log(randomValue1, 'randomValue');
+// const data = randomNums();
+
+// console.log(randomIndex)
+// console.log(data, 'hi');
 
 form.addEventListener('submit', event => {
-    let answer = parseInt(document.getElementById("repopinmodel-verifycode").value);
+    event.preventDefault();
+    let answer = document.getElementById("repopinmodel-verifycode");
     const keyInput = document.getElementById('repopinmodel-pin_code');
 
     console.log(answer.value >= 0);
-    if (answer == "" && keyInput.value == "") {
+    if (answer.value == "" && keyInput.value == "") {
         // console.log("Please add the numbers");
-        randomNums()
+        randomNums(myArray)
         keyInput.style.borderColor = 'red';
         answer.value = ""
         answer.style.borderColor = 'red';
         answer.style.color = 'red';
         errTextCaptcha.style.display = 'inline';
         errText2.style.display = 'inline';
-    } else if (answer == "" && keyInput.value >= 0) {
+        // alert('hi')
+    } else if (answer.value == "" && keyInput.value > 0) {
         errTextCaptcha.style.display = 'inline';
         errText2.style.display = 'disabled';
         errText.style.display = 'disabled';
+        // alert('hi')
     }
-    else if (keyInput.value == "") {
+    else if (answer.value == "") {
         errText2.style.display = 'inline';
         errText.style.display = 'disabled';
         errTextCaptcha.style.display = 'disabled';
+        // alert('kalit bosh')
 
     } else if (keyInput.value == "") {
         errText2.style.display = 'inline';
         errText.style.display = 'disabled';
-        randomNums()
+        randomNums(myArray)
+
     }
-    else if (answer != data.valueIMG ) {
+    else if (answer.value != randomValue1.valueIMG) {
         answer.style.borderColor = 'red';
         answer.style.color = 'red';
-        randomNums()
-        answer = ""
+        // randomNums()
+        answer.value = ""
         answer.style.color = '#000';
         errTextCaptcha.style.display = 'inline';
-        alert('hi')
-
+        alert(data.valueIMG, answer.v)
+        // randomNums();
     } else {
 
         // all good now! //
@@ -90,22 +98,28 @@ form.addEventListener('submit', event => {
                     errText.style.color = 'red';
                     errText2.style.display = 'disabled';
                     errText.style.display = 'inline';
-                    randomNums()
-                    alert('2')
+                    randomNums(myArray)
+                    // alert('2')
                 } else {
                     // console.log('No');
-                    keyInput.style.borderColor = ' #ddd';
-                    errText.style.display = 'disabled';
-                    errTextCaptcha.style.display = 'disabled';
-                    controlLabel.style.color = 'black';
-                    errText2.style.display = 'disabled';
+                    // errText.style.display = 'disabled';
+                    // errTextCaptcha.style.display = 'disabled';
+                    // errText2.style.display = 'disabled';
+                    // errText2.innerHTML= '';
+                    // errText.innerHTML= '';
+                    // errTextCaptcha.innerHTML='';
+                    // controlLabel.style.color = 'black';
                     window.location.href = `https://onlinemarketshop.pythonanywhere.com/doc/${keyInput.value}`;
-                    keyInput.value = ''
-                    answer = ""
                     confirm('Файл загружен!!!');
+                    // keyInput.style.borderColor = ' #ddd';
+                    // keyInput.value = ''
+                    // answer.value = ""
+                    // // randomNums();
+                    randomNums(myArray);
 
 
-                    // location.reload();
+
+                    location.reload();
                 }
             }
             )

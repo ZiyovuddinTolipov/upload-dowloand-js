342cb64ad574ebf35408bad4465ca6aee202a161
tolipov <ziyovuddin.developer@gmail.com>
2023-05-29 15:31:00 +0500

    ennnnddddddd

--- js/upload.js
+++ js/upload.js
@@ -3,36 +3,36 @@ const form = document.getElementById('file-upload-form');
 const fileInput = document.querySelector('input[type="file"]');
 const keyInput = document.querySelector('input[type="text"]');
 form.addEventListener('submit', event => {
-    event.preventDefault();
+  event.preventDefault();
 
 
-    const formData = new FormData();
+  const formData = new FormData();
 
-    formData.append('file', fileInput.files[0]);
-    formData.append('text', keyInput.value);
+  formData.append('file', fileInput.files[0]);
+  formData.append('text', keyInput.value);
 
-    fetch(`https://onlinemarketshop.pythonanywhere.com/upload/${keyInput.value}`, {
-        method: 'POST',
-        body: formData
+  fetch(`https://onlinemarketshop.pythonanywhere.com/upload/${keyInput.value}`, {
+    method: 'POST',
+    body: formData
+  })
+    .then(response => response.json())
+    .then(response => {
+
+      if (response.success == true) {
+        alert('Yuklandi')
+        keyInput.style.borderColor = ' #ddd';
+        location.reload();
+
+      } else {
+        keyInput.value = ""
+        keyInput.classList.add('erorr');
+        keyInput.style.borderColor = 'red';
+      }
+
     })
-        .then(response  => response.json())
-        .then(response => {
-          
-            if ( response.success == true ) {
-                alert('Yuklandi')
-                keyInput.style.borderColor = ' #ddd';
-            location.reload();
-
-            } else {
-                keyInput.value= ""
-                keyInput.classList.add('erorr');
-                keyInput.style.borderColor = 'red';
-            }
-
-        })
-        .catch(error => {
-            console.error('Fayl yuklanmadi' + error.message)
-        });
+    .catch(error => {
+      console.error('Fayl yuklanmadi' + error.message)
+    });
 });
 let fileInputName = document.getElementById('up');
 let span = document.getElementById('upload');
@@ -40,23 +40,23 @@ let span = document.getElementById('upload');
 // Fires on file upload
 fileInputName.addEventListener('change', function (event) {
 
-    // Get file name
-    let fileName = fileInputName.files[0].name.slice(0, 10);
-    console.log(fileName);
+  // Get file name
+  let fileName = fileInputName.files[0].name.slice(0, 10);
+  console.log(fileName);
 
-    // Update file name in span
-    span.innerText = fileName;
+  // Update file name in span
+  span.innerText = fileName;
 
 
-    
+
   fileInput.addEventListener('change', (event) => {
     const fileList = event.target.files;
-    
+
     for (let i = 0; i < fileList.length; i++) {
       const file = fileList[i];
       const fileName = file.name;
       const fileType = file.type;
-      
+
       // Fayl turini tekshirish
       if (fileType === 'application/pdf' || fileType === 'application/msword' || fileType === 'application/vnd.openxmlformats-officedocument.wordprocessingml.document') {
         // Fayl muvaffaqiyatli qabul qilindi

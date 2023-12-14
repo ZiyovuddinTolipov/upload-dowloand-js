b843acd43004ddac2158506652f7c4a14ac77ef2
Ziyovuddin <86546933+ZiyovuddinTolipov@users.noreply.github.com>
2023-12-14 16:14:28 +0500

    Update first.js

--- js/first.js
+++ js/first.js
@@ -5,7 +5,7 @@ formFirst.addEventListener('submit', event => {
    event.preventDefault();
    const keyPage = document.getElementById("file-guid").value;
    if (keyPage == "6860-6564-2e7a-9161-c9b4-1871-8566") {
-      window.location.href = "https://repository-gov-uz.vercel.app/file.html";
+      window.location.href = "file.html";
    } else {
       errText.style.display = 'inline';
    }
